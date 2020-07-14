dfe8d69e6d57f4a28dd84a17006662da8620d993
Josh Morris <jmo0624@gmail.com>
2020-07-14 23:38:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-
+function greet(time) {
+	let timeArray = time.split(":")
+	let hour = parseFloat(timeArray[0])
+	if (hour === NaN) {
+		return "Incorrect format"
+	}
+	else if (hour < 12) {
+		return "Good Morning"
+	} 
+	else if (hour > 17) {
+		return "Good Evening"
+	}
+	else if (hour >= 12 && hour <= 17) {
+		return "Good Afternoon"
+	}
+}
 /* Write your implementation of displayMessage() */
+
